fcc73ed6d3db43e9fdfbe051ae2edae3929eb618
wendlings01@gmail.com <wendlings01@gmail.com>
2021-03-01 23:39:07 -0500

    fleshed out rest of main, added tests, documentation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Setting up and running
 ## Initial setup
 If you don't already have python, then you'll need to get it. I found that there were problems with the Windows app store's install blocking the path to the accessible executable. Also it didn't play well with Git Bash, which is a shame.
+
 Optionally you can set up the python virtual environment using the following on Windows:
 ```powershell
 python -m venv env
@@ -12,9 +13,42 @@ python -m venv env
 source env/Scripts/activate
 ```
 
-Then you'll want to download the dependencies:
+Whether you use the virtual environment or not, you'll want to download the dependencies:
 ```powershell
 pip install requirements.txt
 ```
 
 If those steps all worked then you're ready to run the code. If something failed before this point you likely have some problem with your python install. 
+
+## Running the program
+The main python script takes 2 inputs. The stopwords file and the target text file. Both have been left in the root directory for ease of access.
+
+Running the program on the declaration of independence gives me this:
+```powershell
+(env) PS C:\Users\wendl\twenty-most-common-roots> python .\main.py .\stopwords.txt .\Text1.txt
+['us', 'peopl', 'right', 'govern', 'law', 'state', 'power', 'time', 'among', 'declar', 'establish', 'refus', 'form', 'abolish', 'new', 'coloni', 'assent', 'larg', 'legislatur', 'legisl']
+```
+
+Running it on Alice in Wonderland gives me this:
+```powershell
+(env) PS C:\Users\wendl\twenty-most-common-roots> python .\main.py .\stopwords.txt .\Text2.txt
+['said', 'alic', 'littl', 'look', 'one', 'like', 'know', 'went', 'thought', 'go', 'thing', 'queen', 'time', 'say', 'get', 'see', 'think', 'king', 'turtl', 'head']
+```
+
+# General Overview
+My program takes the two input files and parses them in nearly identical ways. It starts with the stop words, pulls out whitespace delineated words from every line, strips off all remaining non-alphabetical characters, and `tolower`s that result. Then it stems each stop word. I was debating not stemming these, but it simplified the process in my mind and I didn't seem to lose meaningful performance. These stemmed stop words are stored in a dictionary with the stem as the key and their count as the value.
+
+Then the text we care about parsing gets dealt with the same way, except that the stemmed words only get counted if they're not equal to a stemmed stop word. Since this is the same function as for stems, we now have a dictionary of stem:count.
+
+The last step before printing is to order the dictionary and slice off the top 20. Ordering is a one-liner with list comprehensions, the `sorted` function, and a lambda to tell `sorted` to order based off of the value and not whatever the default is for tuples. The choosing the top 20 is just slicing off the first 20 items in the list.
+
+There wasn't a request for a specific output format so I left it printing the bare list object, which is decently readable for me at this size.
+
+# Assumptions
+- We don't need to worry about non-english languages. I started looking at making a test for this, but the data wasn't straight-forward to read and so was deprioritized.
+- Function input will be of the correct type, within reason. No one else is calling these functions for now.
+- Users will use main.py as intended. There's a note within its main function addressing how command line input could be improved for later iterations.
+
+# General Notes
+- My commits got pretty sloppy once I really got into the main function. It's hard to justify cleaning them up on this young of a repo too.
+- I tried to keep comments limited to describing blocks at a high level, noting where I break convention or intentionally cut corners, or further describing tests.
--- test.py
+++ test.py
@@ -2,29 +2,114 @@ import unittest
 import json
 from main import *
 
+# note: no effort was put in to attaching names to test cases
+# this would be a higher priority if this was running a pipeline and it was a 
+# regular task for humans or machines to parse errors
 class Test_convert_file_to_stemmed_word_count_dict(unittest.TestCase):
 
     def test_simple_files(self):
         test_cases = [
+            # white spaces, this could use more types, but spaces and line break seemed like a good place to start
+            {
+                "file_path": "test_data/whitespace.txt",
+                "expected_dict": {"space": 1, "newlin": 1}
+            },
+            # makes sure non-alphabetical characters are removed. this is all of the base characters on my keyboard
+            {
+                "file_path": "test_data/non-alphabeticals.txt",
+                "expected_dict": {"alphabet": 1}
+            },
+            # no input text means no output
             {
                 "file_path": "test_data/empty.txt",
                 "expected_dict": {}
             },
+            # make sure each line gets read
             {
                 "file_path": "test_data/multiple-lines.txt",
                 "expected_dict": {"some": 2, "word":2, "in": 2, "a": 1, "column":2, "with": 1, "repeat": 1, "that":1}
             },
+            # make sure each white-space separated word gets read
             {
                 "file_path": "test_data/single-line.txt",
-                "expected_dict": {"some": 2, "word":2, "in": 2, "a": 1, "row":2, "with": 1, "repeat": 1, "that":1}
-            }
+                "expected_dict": {"some": 3, "word":2, "in": 2, "a": 1, "row":2, "with": 1, "repeat": 1, "that":1}
+            },
+            # make sure the program gracefully handles an invalid file
+            {
+                "file_path": "notAFilePath(O*WPKIJNSEDGD",
+                "expected_dict": {}
+            },
+            # make sure the program gracefully handles a directory
+            {
+                "file_path": "test_data/",
+                "expected_dict": {}
+            },
         ]
 
         for test_case in test_cases:
             actual_dict = convert_file_to_stemmed_word_count_dict(test_case["file_path"])
-            for k,v in test_case["expected_dict"].items():
-                self.assertEqual(v, actual_dict[k], f'expected {v} of {k}, found {actual_dict[k]}.')
-            self.assertEqual(len(actual_dict.keys()), len(test_case["expected_dict"].keys()))
+            # the error reporting here isn't ideal. this test data isn't too large to parse, but if test data becomes
+            # larger, then a better visualization tool like deepdiff might be necessary
+            self.assertEqual(actual_dict, test_case["expected_dict"], 
+                             f'found dict \n{actual_dict} \nnot equal to expected \n{test_case["expected_dict"]}')
+
+    def test_stop_words(self):
+        # the case of no stop_words being passed in is covered by the test_simple_files cases
+        test_cases = [
+            # stop words equivalent to the given text should allow no words to return
+            {
+                "text_file_path": "test_data/multiple-lines.txt",
+                "stop_words": {"some": 2, "word":2, "in": 2, "a": 1, "column":2, "with": 1, "repeat": 1, "that":1},
+                "expected_dict": {}
+            },
+            # stop words should only block words from being counted when they ARE a stop word
+            {
+                "text_file_path": "test_data/multiple-lines.txt",
+                "stop_words": {"some": 2, "word":2, "in": 2, "a": 1, "row":2, "with": 1, "repeat": 1, "that":1},
+                "expected_dict": {"column": 2}
+            }
+        ]
+
+        for test_case in test_cases:
+            actual_dict = convert_file_to_stemmed_word_count_dict(test_case["text_file_path"], test_case["stop_words"])
+            self.assertEqual(actual_dict, test_case["expected_dict"],
+                             f'found dict \n{actual_dict} \nnot equal to expected \n{test_case["expected_dict"]}')
+
+class Test_get_top_20_word_count(unittest.TestCase):
+
+    def test_get_top_20_word_count(self):
+        test_cases = [
+            # an empty input should return an empty list
+            {
+                "input_dict": {},
+                "expected_list": []
+            },
+            # the output should be in descending order with respect to each key's value
+            {
+                "input_dict": {"fred": 1, "wilma": 2, "pebbles": 3},
+                "expected_list": ["pebbles", "wilma", "fred"]
+            },
+            # The output should be limited to 20 items
+            {
+                "input_dict": {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 11:11, 12:12, 13:13, 14:14,
+                               15:15, 16:16, 17:17, 18:18, 19:19, 20:20, 21:21},
+                "expected_list": [21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+            },
+            {
+                "input_dict": {},
+                "expected_list": []
+            },
+        ]
+
+        for test_case in test_cases:
+            ordered_list = get_top_20_word_count(test_case["input_dict"])
+            self.assertEqual(ordered_list, test_case["expected_list"])
+
+    # ints and strings can't be compared, so this input breaks the sorting process
+    def test_throws_when_mixing_value_types(self):
+        mixed_dict = {"number 1":1, "number 2":2, "number 3":3, "string 1":"1", "string 2":"2", "string 3":"3"}
+        with self.assertRaises(TypeError):
+            get_top_20_word_count(mixed_dict)
 
 if __name__ == "__main__":
     unittest.main()
